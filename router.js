00d94d570f2e295af1873a3f6473259958a95e93
mike-stumpf <mike.a.stumpf@gmail.com>
2017-06-18 14:14:54 +0100

    core - added env config package

--- router.js
+++ router.js
@@ -1,7 +1,5 @@
-import React  from 'react';
-import {
-    AppRegistry
-} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 //views
